54a6e17886bce73af6495380bb47fcbd6171a3dc
yogibimbi <yogibimbi@qan.be>
2020-12-10 21:17:44 +0100

    Aaaaand... since we just cleaned ship in app.html, the test cannot find this component anymore and is (rightfully) whining. *sigh*

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('fdx-giphy');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fdx-giphy app is running!');
-  });
 });
